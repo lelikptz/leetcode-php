c5df6041952681e384f61ed97c7fba750273d857
aleksei.orlov <alexey@orlov.tech>
2022-10-22 18:18:29 +0600

    21. Merge Two Sorted Lists https://leetcode.com/problems/merge-two-sorted-lists/

---
+++ src/MergeTwoSortedLists/Solution.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MergeTwoSortedLists;
+
+final class Solution
+{
+    /**
+     * @param ListNode $a
+     * @param ListNode $b
+     * @return ListNode
+     */
+    function mergeTwoLists(ListNode $a, ListNode $b): ListNode
+    {
+        $current = new ListNode();
+        $current->val = 0;
+        $head = $current;
+        while (null !== $a && null !== $b) {
+            if ($a->val > $b->val) {
+                $current->next = $b;
+                $b = $b->next;
+            } else {
+                $current->next = $a;
+                $a = $a->next;
+            }
+            $current = $current->next;
+        }
+
+        $current->next = $a != null ? $a : $b;
+
+        return $head->next;
+    }
+}
