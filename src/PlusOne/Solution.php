a76fc934e0bd25cfd2c34b2a21413d6e52f98595
Alexey Orlov <orlov@proitr.ru>
2022-10-30 18:36:06 +0600

    66. Plus One https://leetcode.com/problems/plus-one/

---
+++ src/PlusOne/Solution.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PlusOne;
+
+final class Solution
+{
+    /**
+     * @param array $digits
+     * @return array
+     */
+    public function plusOne(array $digits): array
+    {
+        for ($i = count($digits) - 1; $i >= 0; $i--) {
+            if ($digits[$i] !== 9) {
+                $digits[$i]++;
+
+                return $digits;
+            }
+            $digits[$i] = 0;
+        }
+
+        return [1, ...$digits];
+    }
+}
