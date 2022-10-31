00d6a1aa38a41034b816fdd26a1f073c619d7f61
Alexey Orlov <orlov@proitr.ru>
2022-10-31 23:42:18 +0600

    766. Toeplitz Matrix https://leetcode.com/problems/toeplitz-matrix/

---
+++ tests/ToeplitzMatrix/SolutionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace tests\ToeplitzMatrix;
+
+use ToeplitzMatrix\Solution;
+use PHPUnit\Framework\TestCase;
+
+class SolutionTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider dataProvider
+     * @param array $matrix
+     * @param bool $expected
+     * @return void
+     */
+    public function plusOne(array $matrix, bool $expected): void
+    {
+        $solution = new Solution();
+
+        $actual = $solution->isToeplitzMatrix($matrix);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            [[[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]], true],
+            [[[1, 2], [2, 2]], false],
+        ];
+    }
+}
