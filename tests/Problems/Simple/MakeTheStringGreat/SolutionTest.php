bbaefaa09624f8d828a3f100e3f2f65dced9402e
lelikptz <alexey@orlov.tech>
2022-11-08 11:36:03 +0600

    1544. Make The String Great https://leetcode.com/problems/make-the-string-great/

---
+++ tests/Problems/Simple/MakeTheStringGreat/SolutionTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace tests\Problems\Simple\MakeTheStringGreat;
+
+use PHPUnit\Framework\TestCase;
+use Problems\Simple\MakeTheStringGreat\Solution;
+
+class SolutionTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider dataProvider
+     * @param string $s
+     * @param string $expected
+     * @return void
+     */
+    public function makeGood(string $s, string $expected): void
+    {
+        $solution = new Solution();
+
+        $actual = $solution->makeGood($s);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            ['leEeetcode', 'leetcode'],
+            ['abBAcC', ''],
+            ['s', 's'],
+            ['Pp', ''],
+        ];
+    }
+}
