49391314c8988aec3288f20eb1554f096c7d1b15
lelikptz <alexey@orlov.tech>
2022-11-18 13:09:12 +0600

    263. Ugly Number https://leetcode.com/problems/ugly-number/

---
+++ src/Problems/Simple/UglyNumber/Solution.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Problems\Simple\UglyNumber;
+
+final class Solution
+{
+    public function isUgly(int $n): bool
+    {
+        if ($n <= 0) {
+            return false;
+        }
+
+        foreach ([2, 3, 5] as $num) {
+            while ($n % $num === 0) {
+                $n /= $num;
+            }
+        }
+
+        return $n === 1;
+    }
+}
