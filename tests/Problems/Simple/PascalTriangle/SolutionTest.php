ff6b7530734e7687be4998014439ea1caf3566bd
lelikptz <alexey@orlov.tech>
2022-11-22 23:12:48 +0600

    118. Pascal's Triangle https://leetcode.com/problems/pascals-triangle/

--- tests/Problems/Simple/PascalTriangle/SolutionTest.php
+++ tests/Problems/Simple/PascalTriangle/SolutionTest.php
@@ -2,25 +2,25 @@
 
 declare(strict_types=1);
 
-namespace tests\Problems\Medium\PerfectSquares;
+namespace tests\Problems\Medium\PascalTriangle;
 
 use PHPUnit\Framework\TestCase;
-use Problems\Medium\PerfectSquares\Solution;
+use Problems\Simple\PascalTriangle\Solution;
 
 final class SolutionTest extends TestCase
 {
     /**
      * @test
      * @dataProvider dataProvider
-     * @param int $n
-     * @param int $expected
+     * @param int $numRow
+     * @param array $expected
      * @return void
      */
-    public function numSquares(int $n, int $expected): void
+    public function minDepth(int $numRow, array $expected): void
     {
         $solution = new Solution();
 
-        $actual = $solution->numSquares($n);
+        $actual = $solution->generate($numRow);
 
         $this->assertEquals($expected, $actual);
     }
@@ -29,9 +29,8 @@ final class SolutionTest extends TestCase
     {
         return
             [
-                [12, 3],
-                [13, 2],
-                [43, 3],
+                [5, [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]],
+                [1, [[1]]],
             ];
     }
 }
